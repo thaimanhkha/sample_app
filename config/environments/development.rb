838ba543d6aa46d54aeed21dd40072f27c546d25
Thai Manh Kha <17520601@gm.uit.edu.vn>
2018-07-10 14:56:36 +0700

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,10 +31,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'example.com' # Don't use this literally; use your local dev host instead
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
